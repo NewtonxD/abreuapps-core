3a7585e5bb024e3d1ca7d3902248d676dd59ef67
NewtonxD <carlosiabreup@gmail.com>
2023-09-15 07:10:16 -0400

    modified:   src/main/java/com/dom/stp/omsa/conf/WebConfig.java 	modified:   src/main/resources/application.properties 	new file:   src/main/resources/static/js/chart.min.js 	new file:   src/main/resources/static/js/kit.fontawesome.155d41b39d.js 	modified:   src/main/resources/static/js/scripts.js 	modified:   src/main/resources/templates/charts.html 	modified:   src/main/resources/templates/fragments/dat_gen_consulta_datos.html 	modified:   src/main/resources/templates/fragments/dat_gen_consulta_grupos.html 	modified:   src/main/resources/templates/index.html 	modified:   src/main/resources/templates/login.html

--- src/main/java/com/dom/stp/omsa/conf/WebConfig.java
+++ src/main/java/com/dom/stp/omsa/conf/WebConfig.java
@@ -4,7 +4,9 @@
  */
 package com.dom.stp.omsa.conf;
 
+import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -22,7 +24,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/content/**")
-            .addResourceLocations("classpath:/static/");
+            .addResourceLocations("classpath:/static/")
+            .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
     }
     
     @Override 
