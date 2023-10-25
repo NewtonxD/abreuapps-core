458b5679ab9726dd0b083bb450846838a801ceed
NewtonxD <carlosiabreup@gmail.com>
2023-10-24 21:23:16 -0400

    async

--- src/main/java/com/dom/stp/omsa/control/general/SSEServ.java
+++ src/main/java/com/dom/stp/omsa/control/general/SSEServ.java
@@ -8,6 +8,7 @@ package com.dom.stp.omsa.control.general;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @Service
 public class SSEServ {
     
+    @Async
     public void emitir(CopyOnWriteArrayList<SseEmitter> emitters,HashMap<String, Object> Datos){
         emitters.forEach(emitter -> {
             try {
