34be44b2956835d1ec493b88872ed53ffa9d2dfe
NewtonxD <carlosiabreup@gmail.com>
2024-02-05 22:49:31 -0400

    lo qeu faltaba

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,12 +1,10 @@
 
 function toggleSidebar() {
-  //$(".toggle-btn").css("display","none");
   $("body").toggleClass("show-sidebar");
 }
 
 function hideSidebar() {
   $("body").removeClass("show-sidebar");
-  //$(".toggle-btn").css("display","block");
 }
 
 
