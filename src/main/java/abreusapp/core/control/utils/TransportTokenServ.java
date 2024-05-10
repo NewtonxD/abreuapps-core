1267a8fa7bf4877dbb6787a6953f5107240481d9
NewtonxD <carlosiabreup@gmail.com>
2024-05-09 20:37:41 -0400

    Empezando Ruta

--- src/main/java/abreusapp/core/control/utils/TransportTokenServ.java
+++ src/main/java/abreusapp/core/control/utils/TransportTokenServ.java
@@ -4,7 +4,6 @@
  */
 package abreusapp.core.control.utils;
 
-import org.springframework.stereotype.Service;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidKeyException;
@@ -17,40 +16,36 @@ import java.util.Base64;
  * @author cabreu
  */
 
-@Service
 public class TransportTokenServ {
     
-    private static final int tokenLength=8;
-    private static final String secretKey="*@3ad_@4%dE*ez";
+    private static final String SECRET_KEY="*@3ad_@4%dE*ez";
     
-    public String generateToken(){
+    public static String generateToken(){
         try {
             Mac hmac = Mac.getInstance("HmacSHA256");
-            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(SECRET_KEY.getBytes(), "HmacSHA256");
             hmac.init(secretKeySpec);
             byte[] tokenBytes = hmac.doFinal(Long.toString(System.currentTimeMillis()).getBytes());
             return Base64.getEncoder().encodeToString(tokenBytes);
         } catch (NoSuchAlgorithmException | InvalidKeyException e) {
-            e.printStackTrace();
             return "";
         }
     }
 
-    private boolean validateToken(String token) {
+    private static boolean validateToken(String token) {
         try {
             Mac hmac = Mac.getInstance("HmacSHA256");
-            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(SECRET_KEY.getBytes(), "HmacSHA256");
             hmac.init(secretKeySpec);
             byte[] tokenBytes = Base64.getDecoder().decode(token);
             byte[] calculatedBytes = hmac.doFinal(Long.toString(System.currentTimeMillis()).getBytes());
             return MessageDigest.isEqual(tokenBytes, calculatedBytes);
         } catch (NoSuchAlgorithmException | InvalidKeyException e) {
-            e.printStackTrace();
             return false;
         }
     }
     
-    public boolean isValidToken(String token){
+    public static boolean isValidToken(String token){
         boolean res=false;
         if(token!=null){
             res=validateToken(token);
