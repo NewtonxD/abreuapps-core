8df9a6ec34482f5bda32fa8ae82df004c0af2feb
NewtonxD <carlosiabreup@gmail.com>
2024-06-13 09:03:36 -0400

    rate limit filter

---
+++ src/main/java/abreusapp/core/conf/SpecificRLFilter.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abreusapp.core.conf;
+
+import jakarta.servlet.annotation.WebFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author cabreu
+ */
+@Component
+@Order(1)
+@WebFilter(urlPatterns = "/API/tiles/*")
+public class SpecificRLFilter extends BaseRLFilter{
+
+    @Override
+    protected int getMaxRequests() {
+        return 2500;
+    }
+    
+}
