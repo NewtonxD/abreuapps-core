37a9c6365dd2b406a44ba9f9ef29d110cb935c6c
NewtonxD <carlosiabreup@gmail.com>
2024-07-26 08:14:37 -0400

    Logos

--- src/main/java/abreusapp/core/conf/SecurityConf.java
+++ src/main/java/abreusapp/core/conf/SecurityConf.java
@@ -9,9 +9,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -31,7 +28,6 @@ public class SecurityConf{
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .addFilterBefore(RL1Filter, LogoutFilter.class)
             .addFilterBefore(LocFilter, SpecificRLFilter.class)
             .authorizeHttpRequests(t -> t
@@ -58,17 +54,6 @@ public class SecurityConf{
             );
 
         return http.build();
-    }
-    
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-            //CorsConfiguration configuration = new CorsConfiguration();
-            //configuration.setAllowedOrigins(Arrays.asList("/**"));
-            //configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-            return source;
-    }
-    
+    }    
     
 }
