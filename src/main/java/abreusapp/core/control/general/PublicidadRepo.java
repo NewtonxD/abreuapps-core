11932823eeb39694d896492dcb3411e0b2263973
NewtonxD <carlosiabreup@gmail.com>
2024-08-07 07:08:02 -0400

    Servicio publicidad

--- src/main/java/abreusapp/core/control/general/PublicidadRepo.java
+++ src/main/java/abreusapp/core/control/general/PublicidadRepo.java
@@ -1,6 +1,8 @@
 package abreusapp.core.control.general;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,5 +11,23 @@ import org.springframework.stereotype.Repository;
  */
 
 @Repository
-public interface PublicidadRepo extends JpaRepository<Publicidad, Long> {    
+public interface PublicidadRepo extends JpaRepository<Publicidad, Long> {
+    @Query(value="SELECT new abreusapp.core.control.general.PublicidadDTO("
+            + "p.id,"
+            + "p.titulo,"
+            + "p.descripcion,"
+            + "p.lg_imagen_video_direccion,"
+            + "p.sm_imagen_video_direccion,"
+            + "p.link_destino,"
+            + "p.fecha_inicio,"
+            + "p.fecha_fin,"
+            + "p.conteo_clic,"
+            + "p.conteo_view,"
+            + "p.activo"
+            + ") FROM Publicidad p order by p.conteo_view asc limit 1")
+    PublicidadDTO customFindAll();
+    
+    @Modifying
+    @Query(value=" update public.pub set cnt_clic=cnt_clic+1 where id=?1 ",nativeQuery = true)
+    void customIncreaseById(Long id);
 }
--- src/main/java/abreusapp/core/control/general/PublicidadServ.java
+++ src/main/java/abreusapp/core/control/general/PublicidadServ.java
@@ -4,7 +4,11 @@ import abreusapp.core.control.usuario.Usuario;
 import jakarta.transaction.Transactional;
 import java.util.Date;
 import java.util.Optional;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,7 +22,10 @@ public class PublicidadServ {
     
     private final PublicidadRepo repo;
     
+    private Long PUBLICIDAD_ACTUAL;
+    
     @Transactional
+    @CacheEvict(value={"Publicidad"}, allEntries = true)
     public void guardar(Publicidad gd, Usuario usuario,boolean existe){
         
         if(existe){ 
@@ -33,5 +40,16 @@ public class PublicidadServ {
     
     public Optional<Publicidad> obtener(Long id){
         return repo.findById(id);
+    }
+    
+    public void IncrementarActual(){
+        repo.customIncreaseById(PUBLICIDAD_ACTUAL);
+    }
+    
+    @Cacheable(value="Publicidad")
+    public PublicidadDTO obtenerUltimo(){
+        PublicidadDTO d=repo.customFindAll();
+        PUBLICIDAD_ACTUAL=d.id();
+        return d;
     }
 }
