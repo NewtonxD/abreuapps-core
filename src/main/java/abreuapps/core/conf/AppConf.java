f7dc1eba10bbf87413d6158d7d1b9d4ab6ac0398
NewtonxD <carlosiabreup@gmail.com>
2024-08-09 14:51:53 -0400

    Publicidad Manager

--- src/main/java/abreuapps/core/conf/AppConf.java
+++ src/main/java/abreuapps/core/conf/AppConf.java
@@ -165,6 +165,7 @@ public class AppConf {
         
         caches.add(buildCache("Publicidad", Duration.ofMinutes(6)));
         caches.add(buildCache("PublicidadArchivo", Duration.ofMinutes(6)));
+        caches.add(buildCache("Publicidades", Duration.ofHours(8)));
         
         SimpleCacheManager cacheManager = new SimpleCacheManager();
         cacheManager.setCaches(caches);
