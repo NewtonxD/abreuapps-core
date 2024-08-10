5b4b9322a3d2080d24ac6cd199a6546c3bb2ebb9
NewtonxD <carlosiabreup@gmail.com>
2024-08-10 09:11:58 -0400

    guardando

--- src/main/java/abreuapps/core/control/PublicidadCntr.java
+++ src/main/java/abreuapps/core/control/PublicidadCntr.java
@@ -1,27 +1,21 @@
 package abreuapps.core.control;
 
-import abreuapps.core.control.transporte.Parada;
-import abreuapps.core.control.transporte.ParadaServ;
+import abreuapps.core.control.general.Publicidad;
+import abreuapps.core.control.general.PublicidadServ;
 import abreuapps.core.control.usuario.AccesoServ;
 import abreuapps.core.control.usuario.Usuario;
 import abreuapps.core.control.utils.DateUtils;
 import abreuapps.core.control.utils.ModelServ;
-import jakarta.servlet.http.HttpServletRequest;
 import java.text.ParseException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -30,8 +24,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/pda")
-public class ParadasCntr {
+@RequestMapping("/pub")
+public class PublicidadCntr {
     
     private final DateUtils FechaUtils;
 
@@ -39,41 +33,76 @@ public class ParadasCntr {
 
     private final ModelServ ModeloServicio;
     
-    private final ParadaServ ParadaServicio;
-        
-//----------------------------------------------------------------------------//
+    private final PublicidadServ PublicidadServicio;
+    
+    @Value("${abreuapps.core.files.directory}")
+    private String FILE_DIRECTORY; 
+    
+    //----------------------------------------------------------------------------//
 //------------------ENDPOINTS PARADAS-----------------------------------------//
 //----------------------------------------------------------------------------//
     @PostMapping("/save")
     public String GuardarParada(
         Model model,
-        Parada paradaCliente,
+        Publicidad publicidad,
+        @RequestParam(name="archivo", 
+                        required = false) MultipartFile archivo,
         @RequestParam(name = "fecha_actualizacionn", 
                         required = false) String fechaActualizacionCliente
     ) throws ParseException {
 
         boolean valido;
         
-        String plantillaRespuesta="fragments/trp_paradas_consulta :: content-default";
+        String plantillaRespuesta="fragments/pub_publicidad_consulta :: content-default";
         
         Usuario u = ModeloServicio.getUsuarioLogueado();
         
         //INICIO DE VALIDACIONES
         String sinPermisoPlantilla = ModeloServicio.verificarPermisos(
-            "trp_paradas_consulta", model, u );
+            "pub_publicidad_consulta", model, u );
         
         //USUARIO NO TIENE PERMISOS PARA EJECUTAR ESTA ACCION
         valido = sinPermisoPlantilla.equals("");
         
+        /*
+        
+            if (file.isEmpty()) {
+                return "File is empty";
+            }
+
+            try {
+                // Generate a new file name with timestamp to avoid duplication
+                String originalFilename = file.getOriginalFilename();
+                String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+                String newFilename = originalFilename.substring(0, originalFilename.lastIndexOf(".")) 
+                        + "_" + timestamp + originalFilename.substring(originalFilename.lastIndexOf("."));
+
+                // Create path for the new file
+                Path path = Paths.get(UPLOAD_DIR, newFilename);
+
+                // Move file to the specific folder
+                Files.copy(file.getInputStream(), path);
+
+                // Save the file path in a variable
+                String savedFilePath = path.toString();
+
+                return "File uploaded successfully: " + savedFilePath;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Failed to upload file";
+            }
+        
+        */
         
         if(valido){
             
-            Optional<Parada> paradaDB = ParadaServicio.obtener(paradaCliente.getId());
+            Optional<Publicidad> publicidadDB = PublicidadServicio.obtener(publicidad.getId());
 
-            if (paradaDB.isPresent()) {
+            if (publicidadDB.isPresent()) {
 
                 if (! FechaUtils.FechaFormato2.format(
-                        paradaDB.get().getFecha_actualizacion()
+                        publicidadDB.get().getFecha_actualizacion()
                         ).equals(fechaActualizacionCliente)
                 ) {
                     
@@ -81,7 +110,7 @@ public class ParadasCntr {
                         "msg",
                         ! ( fechaActualizacionCliente == null || 
                              fechaActualizacionCliente.equals("") ) ? 
-                        "Al parecer alguien ha realizado cambios en la información primero. Por favor, inténtalo otra vez. COD: 00656" :
+                        "Al parecer alguien ha realizado cambios en la información primero. Por favor, inténtalo otra vez. COD: 00686" :
                         "No podemos realizar los cambios porque ya esta Parada se encuentra registrado."
                     );
                     valido = false;
@@ -97,20 +126,20 @@ public class ParadasCntr {
                 if ( ! ( fechaActualizacionCliente == null || 
                         fechaActualizacionCliente.equals("") )
                 ) {
-                    paradaCliente.setFecha_actualizacion(
+                    publicidad.setFecha_actualizacion(
                         FechaUtils.Formato2ToDate(fechaActualizacionCliente)
                     );
                 }
                 
-                if (paradaDB.isPresent()) {
-                    paradaCliente.setFecha_registro(paradaDB.get().getFecha_registro());
-                    paradaCliente.setHecho_por(paradaDB.get().getHecho_por());
+                if (publicidadDB.isPresent()) {
+                    publicidad.setFecha_registro(publicidadDB.get().getFecha_registro());
+                    publicidad.setHecho_por(publicidadDB.get().getHecho_por());
                 }
 
-                ParadaServicio.guardar(paradaCliente, u, paradaDB.isPresent());
+                PublicidadServicio.guardar(publicidad, u, publicidadDB.isPresent());
                 model.addAttribute("msg", "Registro guardado exitosamente!");
 
-                ModeloServicio.load("trp_paradas_consulta", model, u.getId());
+                ModeloServicio.load("pub_publicidad_consulta", model, u.getId());
             }
             
             model.addAttribute("status", valido);
@@ -123,17 +152,17 @@ public class ParadasCntr {
     @PostMapping("/update")
     public String ActualizarParada(
         Model model,
-        @RequestParam("idParada") Integer idParada
+        @RequestParam("idPublicidad") Long idPublicidad
     ) {
         
         boolean valido=true;
-        String plantillaRespuesta="fragments/trp_paradas_registro :: content-default";
+        String plantillaRespuesta="fragments/pub_publicidad_registro :: content-default";
         
         Usuario usuarioLogueado = ModeloServicio.getUsuarioLogueado();
 
-        Optional<Parada> parada = ParadaServicio.obtener(idParada);
+        Optional<Publicidad> publicidadDB = PublicidadServicio.obtener(idPublicidad);
 
-        if (!parada.isPresent()) {
+        if (!publicidadDB.isPresent()) {
 
             //log.error("Error COD: 00637 al editar parada. No encontrado ({})",idParada);
             plantillaRespuesta = "redirect:/error";
@@ -143,55 +172,15 @@ public class ParadasCntr {
         
         //SI TODAS LAS ANTERIORES SON VALIDAS PROCEDEMOS
         if(valido){
-            model.addAttribute("parada", parada.get());
+            model.addAttribute("publicidad", publicidadDB.get());
+            
             model.addAllAttributes(
                     AccesoServicio.consultarAccesosPantallaUsuario(
-                            usuarioLogueado.getId(), "trp_paradas_registro" )
+                            usuarioLogueado.getId(), "pub_publicidad_registro" )
             );
         }
 
         return plantillaRespuesta;
     }
 //----------------------------------------------------------------------------//
-    
-    @PostMapping(value="/getLoc", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity ObtenerLocParada(
-        @RequestParam("idParada") Integer idParada
-    ) {  
-        boolean valido;
-        Usuario u = ModeloServicio.getUsuarioLogueado();
-        
-        //VERIFICAMOS PERMISOS PARA ESTA ACCION
-        String sinPermisoPlantilla = 
-            ModeloServicio.verificarPermisos(
-            "trp_paradas_registro", null, u 
-            );
-        
-        valido = sinPermisoPlantilla.equals("");
-        
-        Map<String, Object> respuesta= new HashMap<>();
-        
-        if(valido){
-            Optional<Parada> LocParada = ParadaServicio.obtener(idParada); 
-            
-            respuesta.put("paradas",ParadaServicio.consultarTodo( 
-                idParada , 
-                true
-            ));
-        
-            //SI TODAS LAS ANTERIORES SON VALIDAS PROCEDEMOS
-            if(LocParada.isPresent()){
-                respuesta.put("lon",LocParada.get().getLongitud());
-                respuesta.put("lat", LocParada.get().getLatitud());
-            }
-        }
-        
-        return new ResponseEntity<>(
-                respuesta.isEmpty() ? null: respuesta,
-                new HttpHeaders(),
-                HttpStatus.OK);  
-    }
-    
-    
 }
